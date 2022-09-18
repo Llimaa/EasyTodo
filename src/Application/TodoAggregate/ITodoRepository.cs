d1979362ed1c7948ed1cac2e51f95706b8891a65
brw.mfatima <brw.mfatima@ifood3rd.com>
2022-09-18 18:35:41 -0300

    finihs repository

--- src/Application/TodoAggregate/ITodoRepository.cs
+++ src/Application/TodoAggregate/ITodoRepository.cs
@@ -5,7 +5,7 @@ public interface ITodoRepository
     Task Raise(Todo todo);
     Task Update(Todo todo);
     Task<Todo> GetById(Guid id);
-    Task<List<Todo>> GetAll();
-    Task<List<Todo>> GetAllByMonth(DateOnly date);
+    Task<IEnumerable<Todo>> GetAll();
+    Task<IEnumerable<Todo>> GetAllByMonth(DateOnly date);
     Task Remove(Guid id);
 }
