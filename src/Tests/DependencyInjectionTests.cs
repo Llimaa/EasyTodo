3743bc52376c1593fad18e5eb85d07426da15b3e
brw.mfatima <brw.mfatima@ifood3rd.com>
2022-11-18 01:04:53 -0300

    adding DependencyInjection Tests

---
+++ src/Tests/DependencyInjectionTests.cs
@@ -0,0 +1,30 @@
+using Application.ErrorBag;
+using EasyTodoTests.Tests;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Microsoft.Extensions.Configuration;
+using Infrastructure.Config;
+using Application.TodoAggregate;
+using Infrastructure.Repositories;
+
+namespace EasyTodoTests;
+
+public class DependencyInjectionTests 
+{
+    [Fact]
+    public void It_Should_Dependence_Inject_As_Transient()
+    {
+        var mockIConfigurationSection = new Mock<IConfigurationSection>();
+        mockIConfigurationSection.Setup(x => x.Key).Returns(nameof(TodoDbConfig));
+        
+        var configurationMock = new Mock<IConfiguration>();
+        configurationMock.Setup(_ => _.GetSection(nameof(TodoDbConfig))).Returns(mockIConfigurationSection.Object);
+
+        var mockedServiceCollection = new Mock<IServiceCollection>();
+
+        mockedServiceCollection.Object.AddTodo(configurationMock.Object);
+
+        mockedServiceCollection.Verify(_ => _.Add(It.Is<ServiceDescriptor>(descriptor =>
+            descriptor.Is<ITodoRepository, TodoRepository>(ServiceLifetime.Transient))));
+    }
+}
