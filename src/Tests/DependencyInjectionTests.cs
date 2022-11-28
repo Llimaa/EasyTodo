ed7ac85e9b7a293a854e601454cac37cdd10ed71
brw.mfatima <brw.mfatima@ifood3rd.com>
2022-11-28 00:02:50 -0300

    feat: add test ti singleton

--- src/Tests/DependencyInjectionTests.cs
+++ src/Tests/DependencyInjectionTests.cs
@@ -1,4 +1,3 @@
-using Application.ErrorBag;
 using EasyTodoTests.Tests;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
@@ -6,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Infrastructure.Config;
 using Application.TodoAggregate;
 using Infrastructure.Repositories;
+using Infrastructure;
 
 namespace EasyTodoTests;
 
@@ -21,10 +21,26 @@ public class DependencyInjectionTests
         configurationMock.Setup(_ => _.GetSection(nameof(TodoDbConfig))).Returns(mockIConfigurationSection.Object);
 
         var mockedServiceCollection = new Mock<IServiceCollection>();
-
         mockedServiceCollection.Object.AddTodo(configurationMock.Object);
 
         mockedServiceCollection.Verify(_ => _.Add(It.Is<ServiceDescriptor>(descriptor =>
             descriptor.Is<ITodoRepository, TodoRepository>(ServiceLifetime.Transient))));
+    }
+
+
+    [Fact]
+    public void It_Should_Dependence_Inject_As_Singleton()
+    {
+        var mockIConfigurationSection = new Mock<IConfigurationSection>();
+        mockIConfigurationSection.Setup(x => x.Key).Returns(nameof(TodoDbConfig));
+        
+        var configurationMock = new Mock<IConfiguration>();
+        configurationMock.Setup(_ => _.GetSection(nameof(TodoDbConfig))).Returns(mockIConfigurationSection.Object);
+
+        var mockedServiceCollection = new Mock<IServiceCollection>();
+        mockedServiceCollection.Object.AddTodo(configurationMock.Object);
+
+        mockedServiceCollection.Verify(_ => _.Add(It.Is<ServiceDescriptor>(descriptor =>
+            descriptor.Is<ITodoDbContext, TodoDbContext>(ServiceLifetime.Singleton))));
     }
 }
